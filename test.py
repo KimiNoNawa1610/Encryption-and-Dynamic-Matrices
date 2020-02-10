63b447e1da4584e4ad0f4809c95732cb05df0a10
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-02-09 20:50:25 -0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,15 @@
+def modExp(b, n, m):
+    binary=""
+    output=1
+    power=1
+    while n > 0:
+        binary=str(n%2)+binary
+        n=int(n/2)
+    power=b
+    for i in binary[::-1]:
+        if(i=="1"):
+            output=(output*power)%m
+        power=(power*power)%m
+        print("Power: ",power,"i: ",i,"output :",output)
+    return output
+print(modExp(3,644,645))
---
+++ test3.py
@@ -0,0 +1,15 @@
+def gcd(a, b):
+    #FIXME: IMPLEMENT THIS METHOD
+    m=0
+    divisor=0
+    if a<=b:
+        m=a
+    elif a>b:
+        m=b
+    i=1
+    while i<=m:
+        if a%i==0 and b%i==0:
+            divisor=i
+        i=i+1
+    return divisor
+print(gcd(414,662 ))
