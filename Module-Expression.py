44d752e519deaa568a35ad829243a8b5d04a9822
Talonss5 <vothanhnhan108@gmail.com>
2020-02-14 23:08:23 -0800

    comments

--- Module-Expression.py
+++ Module-Expression.py
@@ -2,7 +2,7 @@ def modExp(b, n, m):
     binary=""#the power of the number in binary 
     output=1#initialize the output
     power=1#Initial value of output
-    while n > 0:
+    while n > 0:#The while loop
         binary=str(n%2)+binary
         n=int(n/2)
     power=b
