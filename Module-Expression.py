98880adf06f4c246b79ad6bc97b01125f7e6e8b9
Talonss5 <vothanhnhan108@gmail.com>
2020-02-17 21:34:26 -0800

    Update Module-Expression.py

--- Module-Expression.py
+++ Module-Expression.py
@@ -6,3 +6,8 @@ def modExp(b, n, m):
     while(n > 0):#if n is not zero
         binary=str(n%2)+binary#convert the exponent to binary
         n=int(n/2)
+        for i in binary[::-1]:#interate through the binary exponent from right to left
+        if(i=="1"):#if binary exponent component is not zero
+            output=(output*power)%m#update the result 
+        power=(power*power)%m#double the number (b)
+    return output#return the output
