272b50aa17ff6e3f65b8b95c323a1cc6ac5a52e7
Talonss5 <vothanhnhan108@gmail.com>
2020-02-17 21:32:08 -0800

    Update BezoutIdentity.py

--- BezoutIdentity.py
+++ BezoutIdentity.py
@@ -5,3 +5,10 @@ def bezoutCoeffs(a, b):
     sn=0#initialize the n case of the first bezout coefficient
     tn=1#initialize the n case of the second bezout coefficient
     while(b != 0):#while the remainder is not zero
+        m=a//b#(a div b)
+        a1=a#update the subtractor variable 
+        a=b#update the variable being subtracted
+        s1=s#update s1(sn-2)
+        s=sn#update the initial first bezout coefficient
+        t1=t#update t1(tn-2)
+        t=tn#update the initial second bezout coefficient
