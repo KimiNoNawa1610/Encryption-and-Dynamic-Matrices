8c2115107d8c7b06052eab11451f651bbd2a8a39
Talonss5 <vothanhnhan108@gmail.com>
2020-03-06 21:36:25 -0800

    Update Greatest-common-divisor.py

--- Greatest-common-divisor.py
+++ Greatest-common-divisor.py
@@ -1,9 +1,14 @@
 def gcd(a, b):
     #FIXME: IMPLEMENT THIS METHOD
     while(b != 0):#while the remainder is not zero
+        
         m=a//b#(a div b)
+        
         a1=a#update the subtractor variable
+        
         a=b#update the variable being subtracted
+        
         b=a1-m*b#the Eulerian algorithm
+        
     return a#return the result
 
