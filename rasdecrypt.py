2268b1dbcff3ecbc06a4d92d67ffb4a7ecea8d04
Talonss5 <vothanhnhan108@gmail.com>
2020-03-08 22:11:19 -0700

    Update rasdecrypt.py

--- rasdecrypt.py
+++ rasdecrypt.py
@@ -18,6 +18,40 @@ def digits2letters(digits):
     
     return letters
 
+def modinv(a,m):
+    """returns the inverse of a modulo m
+    INPUT: a - integer
+           m - positive integer
+    OUTPUT: a inverse as an integer
+    """
+    c=m
+    s=1
+    t=0
+    sn=0
+    tn=1
+    while(m!=0):
+        b=a//m
+        a1=a
+        a=m
+        s1=s
+        s=sn
+        t1=t
+        t=tn
+        m=a1-b*m
+        sn=s1-b*sn
+        tn=t1-b*tn
+    if(a!=1):
+        
+        return "The given values are not relatively prime"
+    
+    else:
+        
+        while(s<0):
+            s=s+c
+        while(s>c):
+            s=s-c
+        return s 
+    return 0
 
 def decryptRSA(c, p, q, e):
     """decrypts the cipher c, which was encrypted using the key (p * q, e)
@@ -28,5 +62,7 @@ def decryptRSA(c, p, q, e):
     OUTPUT: The decrypted message as a string of letters
     """
     code=c.split(" ")
-    
+    mod=abs((p-1)*(q-1))
+    n=p*q
+    modinv(e,n)
     pass
