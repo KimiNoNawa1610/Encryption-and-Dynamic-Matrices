70425b4b2477387eeb2a4dc10f0e70676c256cc8
Talonss5 <vothanhnhan108@gmail.com>
2020-03-08 20:47:59 -0700

    Update rsaencrypt.py

--- rsaencrypt.py
+++ rsaencrypt.py
@@ -15,3 +15,24 @@ def blocksize(n):
     while int(twofive) < n:
         twofive += "25"
     return len(twofive) - 2
+
+def letters2digits(letters):
+    """converts a string of letters A-Z to a string of double digits without spaces in the range 00-25"""
+    letter2digit = {"A" : "00", "B" : "01", "C" : "02", "D" : "03", "E" : "04", 
+                  "F" : "05", "G" : "06", "H" : "07", "I" : "08", "J" : "09",
+                  "K" : "10", "L" : "11", "M" : "12", "N" : "13", "O" : "14",  
+                  "P" : "15", "Q" : "16", "R" : "17", "S" : "18", "T" : "19",
+                  "U" : "20", "V" : "21", "W" : "22", "X" : "23", "Y" : "24", 
+                  "Z" : "25"}
+        
+    digits = ""  #initializing digits string
+    letters = "".join(letters.split()) #removing whitespaces in the text
+
+    
+    for i in range(0, len(letters)):
+        if(letters[i].isalpha()):
+            letter = letters[i].upper()  #converting to uppercase
+            digit = letter2digit[letter]
+            digits += digit     # concatenating to the string of digits
+    
+    return digits
