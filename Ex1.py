2ee1e26cd75ddd5ee3642414e83ca4dbc349b2d7
Talonss5 <vothanhnhan108@gmail.com>
2020-04-01 19:32:38 -0700

    Update Ex1.py

--- Ex1.py
+++ Ex1.py
@@ -4,16 +4,24 @@ def image2complex(img_name):
     OUT: set of complex numbers x+yi where the intensity of pixel (x,y) is greater than 120
     """
     import image
-    
+    NormalPart=0
+    ComplexPart=0
     # data is a list of lists such that the intensity of pixel (x,y) is given by data[h-1-y][x]
     # where h = height of the image
     data = image.color2gray(image.file2image("img01.png"))
-    
-    
-    #FIXME: COMPLETE THIS FUNCTION
+    output=set()
+    for i in reversed(data):
+            
+        for ii in i:
+            NormalPart+=1
+            if ii<120:
+                output.add(complex(NormalPart,ComplexPart))
+        NormalPart=0
+        ComplexPart+=1
+    return output
+            
+            
+            
 
-from plotting import plot
-import image
-#S = image2complex("img01.png")
-data = image.color2gray(image.file2image("img01.png"))
-print(data)
+
+
