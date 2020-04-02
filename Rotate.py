da84ea03749882ae33b3385d77b0d4799568fb1c
Talonss5 <vothanhnhan108@gmail.com>
2020-04-01 19:32:42 -0700

    Update Rotate.py

--- Rotate.py
+++ Rotate.py
@@ -25,9 +25,5 @@ def rotate(S, theta):
 
 
 
-S = {2 + 2j, 3 + 2j , 1.75 + 1j, 2 + 1j, 2.25 + 1j, 2.5 + 1j, 2.75 + 1j, 3 + 1j, 3.25 + 1j}
 
-print(rotate(S, 1))
-plot(S)
-plot(rotate(S,-math.pi/4))
 
